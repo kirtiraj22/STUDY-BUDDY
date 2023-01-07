36a280042b0b1768e9e2c53ce964fb7ce547a18f
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-01-07 18:40:04 +0530

    added files

---
+++ whiteboard/public/script.js
@@ -0,0 +1,55 @@
+let canvas = document.getElementById("canvas");
+// let test = document.getElementById("test");
+
+canvas.width = 0.98 * window.innerWidth;
+canvas.height = window.innerHeight;
+
+var io = io.connect("http://localhost:8080/");
+
+let ctx = canvas.getContext("2d");
+let x;
+let y;
+let mouseDown = false;
+
+window.onmousedown = (e) => {
+    ctx.moveTo(x, y);
+    io.emit('down', {x, y})
+    mouseDown = true;
+}
+
+window.onmouseup = (e) => {
+    mouseDown = false;
+};
+
+io.on("ondraw", ({x, y}) => {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+});
+
+io.on("ondown", ({ x, y}) => {
+    ctx.moveTo(x , y);
+})
+
+window.onmousemove = (e) => {
+    x = e.clientX;
+    y = e.clientY;
+
+    if(mouseDown){
+        io.emit("draw", { x , y});
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+};
+
+function changered(){
+    ctx.strokeStyle = "red";
+}
+function changeblack(){
+    ctx.strokeStyle = "black";
+}
+function changeyellow(){
+    ctx.strokeStyle = "yellow";
+}
+function changeblue(){
+    ctx.strokeStyle = "blue";
+}
